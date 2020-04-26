0868c7e553b4b4ab7e0384a4fd2bdff32a5706b5
saimaja <saimaakhtar@gmail.com>
2020-04-26 13:55:12 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    #binding.pry
+    complex_sentence = self.split(/[.?!]/).reject {|i| i.empty?}
+    complex_sentence.count
   end
+
 end
